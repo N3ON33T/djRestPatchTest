a488b7b6b6980a9b9dbb6a52544d4087baab0815
navneets <navneets@gmail.com>
2020-10-07 18:07:46 +0530

    Used ModelSerializer instead of Serializer

--- djrestpatch_prj/news/serializers.py
+++ djrestpatch_prj/news/serializers.py
@@ -1,18 +1,7 @@
 from rest_framework import serializers
 from .models import News
 
-class NewsSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    content = serializers.CharField(style={'base_template': 'textarea.html'})
-
-    def create(self, validated_data):
-        # Create new News model instance provided data is valid
-        return News.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        # Update existing News model instance & return it provided data is valid
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.save()
-        return instance
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'content']
